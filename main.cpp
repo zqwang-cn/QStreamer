a716518f771382dbfa2ec110da747e481a2f30e3
zqwang-cn <zqwang.cn@gmail.com>
2020-10-28 09:28:15 +0800

    ctrl-c to quit loop

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include "Pipeline.h"
 #include "utils.h"
+#include <signal.h>
+
+Pipeline* p;
+
+void quit(int s)
+{
+    p->stop();
+    p->finalize();
+}
 
 int main(int argc, char* argv[])
 {
     assert(argc == 2);
+
+    signal(SIGINT, quit);
+
     auto config = load_config_file(argv[1]);
-    Pipeline p(config);
-    p.init();
-    p.run();
+    p = new Pipeline(config);
+    p->init();
+    p->run();
 }
